372d1a1923f71b5a90ab309012157cd98ed2690a
bhatt-gaurav-web <bhattgaurav485@gmail.com>
2024-06-16 00:13:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let facebook = document.querySelector(".Btn1");
+let github = document.querySelector(".Btn2");
+let instagram = document.querySelector(".Btn3");
+let linkdin = document.querySelector(".Btn4");
+
+facebook.addEventListener("click",() => {
+    window.open("https://www.facebook.com/gaurav.go.391");
+});
+
+github.addEventListener("click",() => {
+    window.open("https://github.com/bhatt-gaurav-web");
+});
+
+instagram.addEventListener("click",() => {
+    window.open("https://www.instagram.com/gauravbhatt36/");
+});
+
+linkdin.addEventListener("click",() => {
+    window.open("https://www.linkedin.com/in/gaurav-bhatt-55b54b21b/");
+});
